f7633398b560ce59cb282c6b0195af57f9a9f8c3
Pavel Kuzub <36933133+K4346@users.noreply.github.com>
2023-03-04 02:57:44 +0300

    Update Matrix.kt

--- Matrix.kt
+++ Matrix.kt
@@ -38,32 +38,49 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = MatrixImpl(height, width, e)
 
 /**
  * Реализация интерфейса "матрица"
  */
 
 @Suppress("EqualsOrHashCode")
-class MatrixImpl<E> : Matrix<E> {
+class MatrixImpl<E>(override val height: Int, override val width: Int, private val elements: MutableList<E>) : Matrix<E> {
 
-    override val height: Int = TODO()
+    constructor(height: Int, width: Int, e: E) : this(height, width, MutableList(height * width) { e })
 
-    override val width: Int = TODO()
 
-    override fun get(row: Int, column: Int): E = TODO()
+    override fun get(row: Int, column: Int): E = elements[(row) * width + column]
 
-    override fun get(cell: Cell): E = TODO()
+    override fun get(cell: Cell): E = get(cell.row, cell.column)
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        elements[(row) * width + column] = value
     }
 
-    override fun set(cell: Cell, value: E) {
-        TODO()
+    override fun set(cell: Cell, value: E) = set(cell.row, cell.column, value)
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Matrix<*>) return false
+        if (height != other.height || width != other.width) return false
+        for (row in 0 until height) {
+            for (column in 0 until width) {
+                if (get(row, column) != other[row, column]) return false
+            }
+        }
+        return true
     }
 
-    override fun equals(other: Any?) = TODO()
-
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        var m = ""
+        for (row in 0 until height) {
+            for (column in 0 until width) {
+                print("${get(row, column)} ")
+            }
+			print("\n")
+        }
+        return m
+    }
 }
